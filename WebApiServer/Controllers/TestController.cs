27e5378902e0695c462be81a5488a6967002d7bd
Kuei <ragnakuei@livemail.tw>
2022-06-04 14:03:26 +0800

    Add project files.

---
+++ WebApiServer/Controllers/TestController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiServer.Controllers
+{
+    public class TestController : ApiController
+    {
+        [HttpGet]
+        [Route("api/Test/{teamId}/Channels/{channelId}")]
+        public IHttpActionResult Get(string teamId, string channelId)
+        {
+            return Ok(new
+            {
+                teamId,
+                channelId
+            });
+        }
+
+        [HttpDelete]
+        [Route("api/Test/{teamId}/Channels/{channelId}")]
+        public IHttpActionResult Delete(string teamId, string channelId)
+        {
+            return Ok(new
+            {
+                teamId,
+                channelId
+            });
+        }
+    }
+}
